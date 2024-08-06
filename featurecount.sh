1edc9e98c9ab6a5b8e5002a47ecb91d9ed0ef702
Teesta Naskar <63419282+naskat01@users.noreply.github.com>
2024-08-06 10:45:41 -0400

    featurecount.sh

---
+++ featurecount.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#BSUB -P acc_DADisorders
+#BSUB -q premium
+#BSUB -n 8
+#BSUB -R span[ptile=24]
+#BSUB -R rusage[mem=60000]
+#BSUB -W 120:00
+#BSUB -o stdout.%J.%I
+#BSUB -e stderr.%J.%I
+
+module load R
+
+R CMD BATCH featurecount.R
